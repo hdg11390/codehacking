24910444a59697ec9408427fe204d1cff5fb521d
hdg11390 <hdegroot@staghorn-labs.com>
2021-02-27 11:15:56 -0500

    create user page working without photos

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,12 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Well this is pretty Interesting</h1>
+@extends('layouts.admin')
+
+@section('content')
+<h1>Users</h1>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Active</th>
+        <th>Joined On</th>
+        <th>Updated</th>
+      </tr>
+    </thead>
+    <tbody>
+        @if ($users)
+        @foreach ($users as $user)
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->role->name }}</td>
+            <td>{{ $user->is_active == 1 ? 'Active' : 'Not Active' }}</td>
+            <td>{{ $user->created_at->format('F d, Y') }}</td>
+            <td>{{ $user->updated_at->format('F d, Y') }}</td>
+          </tr>
+            
+        @endforeach
+            
+        @endif
+      
+      
+    </tbody>
+  </table>
+</div>
+
 </body>
 </html>
+
+    
+@endsection
